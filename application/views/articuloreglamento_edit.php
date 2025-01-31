9b3d50a33b64e078d0d0137697680a3e9ece4c33
Stalin francis <educaysoft@gmail.com>
2025-01-30 22:37:38 -0500

    mejoras

--- application/views/articuloreglamento_edit.php
+++ application/views/articuloreglamento_edit.php
@@ -30,13 +30,6 @@ foreach ($reglamentos as $row){
       echo form_textarea('contenido',$articuloreglamento['contenido'],$textarea_options) ?></td>
   </tr>
 
-    <td>Archivo:</td>
-<td><?php 
-	$textarea_options = array('class' => 'form-control','rows' => '2',   'cols' => '20', 'style'=> 'width:50%;height:100px;', "placeholder"=>"contenido del articuloreglamento" );    
-      echo form_textarea('archivo',$articuloreglamento['archivo'],$textarea_options) ?></td>
-  </tr>
-
-
 
  <tr>
  <td colspan="2"> <hr><?php echo form_submit('submit', 'Guardar'); ?> <?php echo anchor('articuloreglamento','Atras') ?></td>
