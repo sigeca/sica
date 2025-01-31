ddb76214350b88dceecf3c27a483c345569b8023
Stalin francis <educaysoft@gmail.com>
2025-01-31 00:41:09 -0500

    mejoreas

--- application/views/articuloreglamento_genpagina.php
+++ application/views/articuloreglamento_genpagina.php
@@ -385,10 +385,7 @@ foreach($articuloreglamentos as $row){
  	$inicio=0;
 		}
 
-  $data=$data.'	<div class="album py-5 bg-light">
-    		<div class="container">
-      		<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">';
-	
+
     
 
 $data=$data.'<div class="col">
