c223692cf5422e7c79e11f1788b85a01b2b0c7cf
Stalin francis <educaysoft@gmail.com>
2025-01-31 00:33:14 -0500

    mejoreas

--- application/views/articuloreglamento_genpagina.php
+++ application/views/articuloreglamento_genpagina.php
@@ -381,7 +381,7 @@ foreach($articuloreglamentos as $row){
         	<p style="font-size:2em; color:#6c757d;text-shadow: 1px 1px 2px #000, -1px -1px 2px #fff;">Reglamentos institucionales</p>
       		</div>
     		</div>
-  		</section>;
+  		</section>
   		<div class="album py-5 bg-light">
     		<div class="container">
       		<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">';
