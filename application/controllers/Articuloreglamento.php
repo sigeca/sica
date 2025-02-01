3c8b963d54561fc448ce6ca716797fdd3684fcbd
Stalin francis <educaysoft@gmail.com>
2025-02-01 10:40:37 -0500

    memlor

--- application/controllers/Articuloreglamento.php
+++ application/controllers/Articuloreglamento.php
@@ -109,7 +109,6 @@ function articuloreglamento_data()
 	 	$data0 = $this->articuloreglamento_model->lista_articuloreglamentosA();
 		$data=array();
 
-        print_r($data0);
 		foreach($data0->result() as $r){
 			$data[]=array($r->idarticuloreglamento,$r->elreglamento,$r->numero,$r->titulo,$r->contenido,
 				$r->href='<a href="javascript:void(0);" class="btn btn-info btn-sm item_ver"  data-retorno="'.site_url('articuloreglamento/actual').'"  data-idarticuloreglamento="'.$r->idarticuloreglamento.'">Ver</a>');
