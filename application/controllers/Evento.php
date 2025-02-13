0e1a279e9185cd12e09385011131e8532422dad9
Stalin francis <educaysoft@gmail.com>
2025-02-12 21:36:43 -0500

    mejoras

--- application/controllers/Evento.php
+++ application/controllers/Evento.php
@@ -395,7 +395,7 @@ public function cumplimiento(){
 
 		$data['filtro']= $idpersona; //$data['participante']['idparticipante'];
 		}
-
+        die("llago hasta aqui");
 		$data['title']="Evento";
 		$this->load->view('template/page_header');		
 		$this->load->view('evento_xpersona',$data);
