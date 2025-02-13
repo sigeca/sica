9a7bc011253445e89dacc4ee4bdf4411b6996475
Stalin francis <educaysoft@gmail.com>
2025-02-12 22:55:06 -0500

    mejoras

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -370,12 +370,12 @@ if ((bool)$result === TRUE) {
 //	$idevento = $this->input->post('idevento');
 //	$result = $this->login_model->read_user_information($email,$password,$idevento);
 	$result = $this->login_model->read_user_information($email,$password);
+
 	if ((bool)$result != false) {
 	// Se busca la información del dueño del usuario.
 		$result2 = $this->login_model->get_persona($result[0]->idpersona);
-		if ((bool)$result2 != false) {
+		if (!empty($result2 )) {
       			$resulti = $this->institucion_model->get_institucion($result[0]->idinstitucion);
-                die("carga assion data");
 			$session_data = array(
 				'email' => $result[0]->email,
 				'idusuario' => $result[0]->idusuario,
@@ -391,7 +391,7 @@ if ((bool)$result === TRUE) {
 		
 		$result3 = $this->acceso_model->get_usuario($result[0]->idusuario);
 
-		if ((bool)$result3 != false) {
+		if ( !empty($result3)) {
 			$accesos = array();
 			foreach($result3 as $row)
 			{
