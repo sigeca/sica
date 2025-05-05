3c95dc30c24dbce6322281f8006b77adb8f14412
Stalin francis <educaysoft@gmail.com>
2025-05-05 07:21:57 -0500

    mejoras

--- application/models/Sesionevento_model.php
+++ application/models/Sesionevento_model.php
@@ -30,7 +30,7 @@ class Sesionevento_model extends CI_model {
 		 if($idevento>0)
                 {
                 $this->db->where('idevento='.$idevento);
-                $query = $this->db->get('sesionevento0'); // Suponiendo que la tabla se llama 'documentos'
+                $query = $this->db->get('sesionevento1'); // Suponiendo que la tabla se llama 'documentos'
         return $query->result_array(); // Devolver array de documentos
          }else{
 
