ea6c3fc4be8b0c6ff9c4258d8f615bce67286cfb
Stalin francis <educaysoft@gmail.com>
2025-05-24 15:51:46 -0500

    mejoras

--- application/controllers/Pagoevento.php
+++ application/controllers/Pagoevento.php
@@ -156,7 +156,7 @@ public function listar()
 
  public function pagoevento_flutter()
     {
-		$idevento=$this->input->get('idevento');
+		$idevento=$this->input->post('idevento');
 	 	$pagoeventos=$this->pagoevento_model->get_pagoeventopersona($idevento,0);
         echo json_encode(['data' => $pagoeventos]); // Devolver datos en formato JSON
  	}
