c6afdb6cd4bfe03fee56f9a499b53117e004b8fb
tenorio <anthony.tenorio.espinoza@utelvt.edu.ec>
2025-06-30 16:12:11 -0500

    mejoras

--- application/controllers/Evento.php
+++ application/controllers/Evento.php
@@ -429,8 +429,6 @@ public function cumplimiento(){
 			$data['persona']=$this->persona_model->persona($this->uri->segment(3))->result();
 		    $data['filtro']= $idpersona; //$data['participante']['idparticipante'];
 		}
-        print_r($data['persona']);
-        die();
 		$data['title']="Evento";
 		$this->load->view('page_header',array('session_data' => $this->session->userdata())  );		
 		$this->load->view('evento_xpersona',$data);
