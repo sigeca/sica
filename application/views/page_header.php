cb42455f0ca89d2596ec9d3e2acb7a0079d2ccc8
tenorio <anthony.tenorio.espinoza@utelvt.edu.ec>
2025-06-30 19:11:03 -0500

    mejoras

--- application/views/page_header.php
+++ application/views/page_header.php
@@ -22,15 +22,20 @@
     <meta name="description" content="Desarrollamos soluciones de software para la gestión académica, administrativa y de investigación.">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4SsQ4Cbl8SglMTtHvFuBVGmQ+jLge7Fiab+xlWiC5NWFy1QeLAG+fb6G/oB5FcMZeFgYsVkPekCPZkd1Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <link rel="stylesheet" href="<?php echo base_url('assets/css/main.css'); ?>">
     <link rel="stylesheet" href="<?php echo base_url('assets/css/responsive.css'); ?>">
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZQL/MVP6ubffg0lP5tQbdlHM2ddjU/msMgrtgQYcPu4kw3FqE2yaCEvA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+<!---    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>  -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css"/>
+<script type="text/javascript" src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+
+
+
 </head>
 <body>
     <?php if (isset($this->session->userdata['logged_in'])) : ?>
