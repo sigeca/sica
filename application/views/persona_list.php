e35336143b7ea27ccb8c681bcb34ba39c4e6c3db
tenorio <anthony.tenorio.espinoza@utelvt.edu.ec>
2025-07-03 19:48:42 -0500

    mejoras

--- application/views/persona_list.php
+++ application/views/persona_list.php
@@ -87,7 +87,9 @@ $(document).ready(function(){
     var mytabla= $('#mydatac').DataTable({
             "processing": true,
             "serverSide": true, // If using server-side processing for large datasets
-            "ajax": {url: '<?php echo site_url('persona/persona_data')?>', type: 'GET'},});
+"ajax": {url: '<?php echo site_url('persona/persona_data')?>', type: 'GET'},
+"responsie": true 
+});
 
 });
 
