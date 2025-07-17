b93c31a71748440dec06a04af2ec44e0e84199b1
tenorio <anthony.tenorio.espinoza@utelvt.edu.ec>
2025-07-17 10:04:36 -0500

    mejoras

--- application/views/validarcorreo_form.php
+++ application/views/validarcorreo_form.php
@@ -49,6 +49,14 @@
 
         /* Left Section: Event Presentation */
         #presentacion {
+             flex-grow: 0;   /* Evita que el item crezca para ocupar espacio extra */
+             flex-shrink: 1; /* Permite que el item se encoja si no hay suficiente espacio */
+            flex-basis: auto; /* Permite que el item tome el tamaño de su contenido inicial */
+    /* Puedes usar 'flex: 0 1 auto;' como una abreviatura para las tres propiedades anteriores */
+    
+             width: auto; /* Asegura que el ancho se ajuste al contenido, no al 100% */
+    
+
             background-color: #ffffff;
             padding: 20px;
             border-radius: 10px;
