fc38a7bce2cb34709fdb756e4e6a2ecaa41cd2d2
tenorio <anthony.tenorio.espinoza@utelvt.edu.ec>
2025-07-17 05:44:25 -0500

    mejoras

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -60,7 +60,7 @@ public function validarcorreo()
 		$data['idevento']=54; 
 
 	}
-	$this->load->view('template/page_header.php');
+	$this->load->view('page_header.php');
 	$this->load->view('validarcorreo_form',$data);
 //	$this->load->view('template/page_footer.php');
 
