4fc87049bd88bf85527ec4f8aa8260a1c9962b41
tenorio <anthony.tenorio.espinoza@utelvt.edu.ec>
2025-07-30 17:06:15 -0500

    mejoras

--- application/models/IaPrompt_model.php
+++ application/models/IaPrompt_model.php
@@ -5,9 +5,9 @@ class IaPrompt_model extends CI_Model {
         return $this->db->get('reglamento')->result();
     }
 
-    public function get_contenido_articulo($idreglamento, $numeroarticulo) {
+    public function get_contenido_articulo($idreglamento, $numero) {
         $this->db->where('idreglamento', $idreglamento);
-        $this->db->where('numeroarticulo', $numeroarticulo);
+        $this->db->where('numero', $numero);
         $query = $this->db->get('articuloreglamento');
         $row = $query->row();
         return $row ? $row->contenido : "Artículo no encontrado.";
