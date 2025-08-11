83b3913a51f3df0639e1097ed44098585ea07299
tenorio <anthony.tenorio.espinoza@utelvt.edu.ec>
2025-08-11 10:38:33 -0500

    update

--- application/controllers/Evento.php
+++ application/controllers/Evento.php
@@ -305,10 +305,9 @@ public function cumplimiento(){
 		$data['asignaturadocentes'] = $this->asignaturadocente_model->lista_asignaturadocentesA(0)->result();
 		$data['calendarioacademicos'] = $this->calendarioacademico_model->lista_calendarioacademicosA(0)->result();
 		$data['jornadadocente'] =$this->jornadadocente_model->jornadadocentes($data['evento']['idasignaturadocente'])->result();
-		$data['title']="<div style='padding:30px; text-align:left; background:black; color:white; font-size:30px;'> <p style='font-size:40px; font-weight:bold; text-transform:uppercase;' >Detalle del evento # ". $data['evento']['idevento']."</p>
-		<p>En este formulario esta toda la información de evento que este dirigiendo</p> </div>";
 
 
+		$data['title']="Detalle del evento # ". $data['evento']['idevento'];
 
 
 		$this->load->view('page_header');		
