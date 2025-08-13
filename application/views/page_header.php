0144abc0879e5e20241bfe81755c409dca525f5b
tenorio <anthony.tenorio.espinoza@utelvt.edu.ec>
2025-08-13 08:41:51 -0500

    update

--- application/views/page_header.php
+++ application/views/page_header.php
@@ -29,7 +29,7 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZQL/MVP6ubffg0lP5tQbdlHM2ddjU/msMgrtgQYcPu4kw3FqE2yaCEvA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <!---    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>  -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script> -->
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css"/>
 <script type="text/javascript" src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
