aa430fb0635418aea6824dc8a6f40cdb0522aa99
tenorio <anthony.tenorio.espinoza@utelvt.edu.ec>
2025-08-14 22:19:23 -0500

    update

--- application/views/evento_record.php
+++ application/views/evento_record.php
@@ -382,45 +382,46 @@ echo form_input($arrdatos) ?>
 
 
 
-<div class="form-group row">
-	<div class="col-md-10">
-	<div class="row justify-content-left">
-      	<!-- Page Heading -->
- 	<div class="row">
-  	<div class="col-12" style="border:solid;">
-
-<div class="row" style="background-color:lightgray; padding-top:0.5cm; padding-bottom:0.5cm; border-bottom:0.5cm solid white;">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <b>Sesiones dictadas: </b>
-        </div>
-        <div class="pull-right">
-            <a class="btn btn-success" href="<?php echo base_url('sesionevento/add/'.$evento['idevento']) ?>">Nueva sesion</a><a class="btn btn-danger" onclick='reportepdf()' >Reporte</a>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <div class="row justify-content-left">
+                <div class="row">
+                    <div class="col-12" style="border:solid;">
+                        <div class="row" style="background-color:lightgray; padding-top:0.5cm; padding-bottom:0.5cm; border-bottom:0.5cm solid white;">
+                            <div class="col-lg-12 margin-tb">
+                                <div class="pull-left">
+                                    <b>Sesiones dictadas: </b>
+                                </div>
+                                <div class="pull-right">
+                                    <a class="btn btn-success" href="<?php echo base_url('sesionevento/add/'.$evento['idevento']) ?>">Nueva sesion</a>
+                                    <a class="btn btn-danger" onclick='reportepdf()' >Reporte</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="table-responsive">
+                            <table class="table table-striped table-bordered table-hover w-100" id="mydatac">
+                                <thead>
+                                    <tr>
+                                        <th>sesion</th>
+                                        <th>unidad</th>
+                                        <th>fecha</th>
+                                        <th>inicio</th>
+                                        <th>termino</th>
+                                        <th>Eval</th>
+                                        <th>tema tratado</th>
+                                        <th style="text-align: right;">Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="show_data">
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-</div>
-        <div class="table-responsive">
-	<table class="table table-striped table-bordered table-hover" id="mydatac">
-	 <thead>
-	 <tr>
-	 <th>sesion</th>
-	 <th>unidad</th>
-	 <th>fecha</th>
-	 <th>inicio</th>
-	 <th>termino</th>
-	 <th>Eval</th>
-	 <th>tema tratado</th>
-	 <th style="text-align: right;">Actions</th>
-	 </tr>
-	 </thead>
-	 <tbody id="show_data">
-	 </tbody>
-	</table>
-	</div>
-	</div>
-	</div>
-	</div>
-	</div> 
 </div>
 
 
