8111a62c0b94b89bfd53126eed5995f1a3bda9dc
tenorio <anthony.tenorio.espinoza@utelvt.edu.ec>
2025-08-17 07:29:52 -0500

    update

--- application/views/evento_xpersona.php
+++ application/views/evento_xpersona.php
@@ -74,17 +74,8 @@ body {font-family: Arial, Helvetica, sans-serif;}
 </div>
 </div>
 </div>
-</div>
-</div>
-
-<div class="modal fade" id="Modal_pdf" tabindex="-1"  role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" style="height: 800px;">
 
 
- <div class="modal-footer">
-<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-</div>
-
- </div>
 
 
 
