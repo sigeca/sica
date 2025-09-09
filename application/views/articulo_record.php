8d50a465f0032b45635833ee546406f42e6d10f7
tenorio <anthony.tenorio.espinoza@utelvt.edu.ec>
2025-09-08 23:30:54 -0500

    mejoras

--- application/views/articulo_record.php
+++ application/views/articulo_record.php
@@ -77,7 +77,7 @@ $textarea_options = array('class' => 'form-control','rows' => '4','disabled'=>'d
 <div class="form-group row">
     <label class="col-md-2 col-form-label"> Foto:</label>
 	<div class="col-md-10">
- <img src="https://educaysoft.org/repositorioeys/articulos/articulo<?php echo $articulo['idarticulo']; ?>.jpg" alt="articulo" width="400" height="300"> 
+ <img src="https://educaysoft.org/descargar.php?archivo=articulos/articulo<?php echo $articulo['idarticulo']; ?>.jpg" alt="articulo" width="400" height="300"> 
   
 
 	</div> 
